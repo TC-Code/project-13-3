e4238fc3a45510a8b0f55b131a6217bc3837376a
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-04-10 22:13:54 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,12 +1,9 @@
 process.stdin.setEncoding("utf-8");
 process.stdin.on("readable", function() {
   var input = process.stdin.read();
-  if (input !== null) {
-    var instruction = input.toString().trim();
-  } else {
-    return;
-  }
 
+  if (!input) return;
+  var instruction = input.toString().trim();
   switch (instruction) {
     case "/exit":
       process.stdout.write("Quitting app!\n");
